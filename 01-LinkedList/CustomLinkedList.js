bcaf46fc7a01c7bf794dc0cad6f9f77b247096d8
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-20 23:46:54 -0500

    custom linked list

--- 01-LinkedList/CustomLinkedList.js
+++ 01-LinkedList/CustomLinkedList.js
@@ -27,8 +27,52 @@ class LinkedList {
     }
 
     // Insert last node
-    insertTail(node) {
+    insertTail(data) {
+        if (this.head === null) throw new Error('LinkedList is empty');
+        let node = new Node(data);
+        let current = this.head;
 
+        while(current !== null){
+            if(current.next === null){
+                current.next = node;
+                break;
+            }
+            current = current.next;
+        }
+    }
+
+    // Remove at index
+    removeNodeAtIndex(index){
+        if (this.head === null) throw new Error('LinkedList is empty');
+        let count = 0;
+        let current = this.head;
+        let previous = this.head;
+
+        while(current !== null){
+            if (count === index){
+                previous.next = current.next;
+            }
+            previous = current;
+            current = current.next;
+            count++;
+        }
+    }
+
+    // Clear list
+    clearList(){
+        this.head = null;
+    }
+
+    // Print list data
+    printListData(linkedList) {
+        if (linkedList.head === null) throw new Error('LinkedList is empty');
+        let str = '';
+        let current = linkedList.head;
+        while (current !== null) {
+            str = str + `[${current.data}] -> `;
+            current = current.next;
+        }
+        console.log(str.slice(0,(str.length - 3)));
     }
 
     // Get at index
@@ -53,6 +97,13 @@ const lList = new LinkedList();
 lList.insertHead(300);
 lList.insertHead(200);
 lList.insertHead(100);
+lList.insertTail('test');
+lList.insertTail('this is tail');
+
+lList.removeNodeAtIndex(3);
+// lList.clearList();
 
 
 console.log(lList.getNodeAtIndex(0));
+
+lList.printListData(lList);
