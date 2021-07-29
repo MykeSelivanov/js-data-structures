bce725b926bcf4ab455c9c82cd4c73cffd0cee21
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-28 22:02:46 -0500

    priority queue push

--- 03-Queue/PriorityQueue.js
+++ 03-Queue/PriorityQueue.js
@@ -1,51 +1,54 @@
 // Priority Queue implementation with Linked List
 
 class Node {
-    // lower values implement higher priority
-    constructor(data, priority){
+    // Lower values indicate higher priority
+    constructor(data, priority) {
         this.data = data;
         this.priority = priority;
         this.next = null;
     }
 }
 
-// descending order high => priority => low
-
 class PriorityQueue {
-    constructor(){
-        this.head = null;
+    constructor(data, priority) {
+        var temp = new Node();
+        temp.data = data;
+        temp.priority = priority;
+        temp.next = null;
+        temp.head = null;
     }
-
-    // push
-    pushNode(data, priority){
-        let newNode = new Node(data, priority);
-        
-        // check if priority queue is empty
-        if (this.head === null) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            let previous = this.head;
-            while (current !== null){
-                if (current.priority < newNode.priority){
-                    previous = current;
-                    current = current.next;
-                } else if (current.priority > newNode.priority){
-                    previous.next = newNode;
-                    newNode.next = current;
-                }
-            }
+    push(head, d, p) {
+        var start = head;
+        var temp = new Node(d, p);
+        if (head === null) {
+            head = temp;
         }
-        return this;
+        //  console.log(start);
+        if (head.priority > p) {
+            // Insert New Node before head
+            temp.next = head;
+            head = temp;
+        } else {
+            // console.log(head);
+            // Traverse the list and find a
+            // position to insert new node
+            while (start.next != null && start.next.priority < p) {
+                start = start.next;
+            }
+            // Either at the ends of the list
+            // or at required position
+            temp.next = start.next;
+            start.next = temp;
+        }
+        return head;
     }
-
-    // pop
-
-    // peek
-
 }
-
-let pq = new PriorityQueue();
-pq.pushNode(4,4);
-pq.pushNode
-
+// Driver code
+// Create a Priority Queue
+// 7.4.5.6
+var pq = new PriorityQueue(4, 1);
+pq = pq.push(pq, 4, 1);
+pq = pq.push(pq, 5, 2);
+pq = pq.push(pq, 6, 3);
+pq = pq.push(pq, 7, 0);
+console.log(pq)
