6d2c8541b64e2b53c638ad777fd65f1ca69df843
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-30 22:10:41 -0500

    bst

--- 04-Trees/01-Binary-Search-Tree/CustomBinarySearchTree.js
+++ 04-Trees/01-Binary-Search-Tree/CustomBinarySearchTree.js
@@ -1,48 +1,69 @@
 class Node {
-    constructor(data){
+    constructor(data) {
         this.data = data;
         this.left = null;
         this.right = null;
     }
 }
-
-// create Binary Search tree
-class BST{
+// create Binary Search Tree
+class BST {
     constructor() {
         this.root = null;
     }
-
     // insert
-    insert(data){
+    insert(data) {
         let newNode = new Node(data);
         // check if tree is empty
-        if (this.root === null) {
+        if (this.isEmpty()) { // this.root === null
             this.root = newNode;
             return this;
         }
         let temp = this.root;
-
-        while(){
-            // if same data exists 
+        while (true) {
+            // if same data exists
             if (newNode.data === temp.data) return undefined;
-
-            if(newNode.data < temp.data){
-                if(temp.data === null){
+            // check if new node data is 
+            // bigger than or smaller than temp.data
+            if (newNode.data < temp.data) {
+                if (temp.left === null) {
                     temp.left = newNode;
                     return this;
                 }
+                temp = temp.left;
+            } else {
+                if (temp.right === null) {
+                    temp.right = newNode;
+                    return this;
+                }
+                temp = temp.right;
             }
         }
-
     }
-
     // check if empty
-    
-
-
+    isEmpty() {
+        return this.root ? false : true;
+    }
+    contains(data) {
+        if (this.isEmpty()) return false;
+        let temp = this.root;
+        while (temp) {
+            if (data < temp.data) {
+                temp = temp.left;
+            } else if (data > temp.data) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
-
 // init
 let bst = new BST();
 bst.insert(10);
+bst.insert(8);
+bst.insert(11);
+bst.insert(9);
 console.log(bst);
+console.log(bst.isEmpty());
+console.log(bst.contains(8));
