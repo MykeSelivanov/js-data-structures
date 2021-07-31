9900a91a76c229cf54ed113d9d2e9b32b39544cf
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-30 21:47:44 -0500

    bst

--- 04-Trees/01-Binary-Search-Tree/CustomBinarySearchTree.js
+++ 04-Trees/01-Binary-Search-Tree/CustomBinarySearchTree.js
@@ -7,3 +7,42 @@ class Node {
 }
 
 // create Binary Search tree
+class BST{
+    constructor() {
+        this.root = null;
+    }
+
+    // insert
+    insert(data){
+        let newNode = new Node(data);
+        // check if tree is empty
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+        let temp = this.root;
+
+        while(){
+            // if same data exists 
+            if (newNode.data === temp.data) return undefined;
+
+            if(newNode.data < temp.data){
+                if(temp.data === null){
+                    temp.left = newNode;
+                    return this;
+                }
+            }
+        }
+
+    }
+
+    // check if empty
+    
+
+
+}
+
+// init
+let bst = new BST();
+bst.insert(10);
+console.log(bst);
