0c0471212ffa004f58fae73fc84716c062e60b6a
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-10 20:30:52 -0500

    recursion

---
+++ 06-Recursion/FactorialOfN.js
@@ -0,0 +1,16 @@
+// calculate the factorial of a number
+
+function factorial(num) {
+    // base
+    if (num < 0) throw new Error('Num could not be negative!');
+    if (num === 0) {
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
+}
+
+console.log(factorial(4)); // 24
+console.log(factorial(5)); // 120
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
---
+++ 06-Recursion/MultiplyNums.js
@@ -0,0 +1,10 @@
+// Write a function to multiply(a,b)
+// where a and b are both positive integers
+// you can only use + or - operators
+
+function multiply(x, y) {
+    // base
+    if (y === 0) return 0;
+    
+    return x + multiply(x, y-1);
+}
