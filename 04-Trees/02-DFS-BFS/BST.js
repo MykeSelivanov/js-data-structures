8fabab429b64c8225bb710945060ccc76c73c455
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-11 20:59:28 -0500

    BFS

--- 04-Trees/02-DFS-BFS/BST.js
+++ 04-Trees/02-DFS-BFS/BST.js
@@ -37,9 +37,50 @@ class BST {
             }
         }
     }
+
+    BFS() {
+        // iteratively
+        // create a var current to store a root node
+        let currentNode = this.root;
+        // 2 arrays
+        // 1 - results = []
+        let results = [];
+        // 2 - queue = []
+        let queue = [];
+
+        // push current to queue
+        queue.push(currentNode);
+        // loop until queue.length is true
+        while(queue.length) {
+            // current is equal to queue.shift()
+            currentNode = queue.shift();
+            // push current value to results
+            results.push(currentNode.value);
+            // check if current has left node, push current.left to the queue
+            if (currentNode.left) {
+                queue.push(currentNode.left);
+            }
+            // check if current has right node, push current.right to the queue
+            if (currentNode.right) {
+                queue.push(currentNode.right);
+            }
+        }
+        return results;
+        // return results array at the end
+    }
 }
 
 let bst = new BST();
-console.log(bst.insert(3));
-console.log(bst.insert(2));
-console.log(bst.insert(5));
+console.log(bst.insert(10));
+console.log(bst.insert(8));
+console.log(bst.insert(6));
+console.log(bst.insert(9));
+console.log(bst.insert(12));
+console.log(bst.insert(11));
+console.log(bst.insert(13));
+
+//          10
+//      8        12
+//   6    9   11    13
+
+console.log(bst.BFS());
