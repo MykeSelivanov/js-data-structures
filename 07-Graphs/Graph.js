f99551e88c467ef51de35d35f61e4f176a3dfc45
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-17 21:17:38 -0500

    graph

--- 07-Graphs/Graph.js
+++ 07-Graphs/Graph.js
@@ -1,3 +1,40 @@
 class Graph {
-    
+    constructor() {
+        this.adjacencyList = {};
+    }
+    addVertex(vertex) {
+        if(!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+            return true;
+        }
+        return false;
+    }
+
+    addEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1].push(vertex2);
+            this.adjacencyList[vertex2].push(vertex1);
+            return true;
+        }
+        return false;
+    }
+
+    removeEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(vtx => vtx !== vertex2);
+            this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(vtx => vtx !== vertex1);
+            return true;
+        }
+        return false;
+    }
 }
+
+let graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'C');
+graph.removeEdge('A','B');
+console.log(`graph`, graph);
