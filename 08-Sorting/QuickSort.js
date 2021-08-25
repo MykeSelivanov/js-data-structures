24ad46ffc85b9031fc24fa05f7f8c7ecbff212bf
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-24 23:56:29 -0500

    quick sort

--- 08-Sorting/QuickSort.js
+++ 08-Sorting/QuickSort.js
@@ -5,5 +5,19 @@ function quickSort(array) {
 
 function quiickSortHelper(array, startIdx, endIdx) {
     if (startIdx >= endIdx) return array;
-    
+    const pivot = startIdx;
+    let leftPointer = startIdx + 1;
+    let rightPointer = endIdx;
+
+    while (rightPointer >= leftPointer) {
+        if (array[leftPointer] > array[pivot] && array[rightPointer] < array[pivot]) {
+            swap(leftPointer, rightPointer, array);
+        }
+    }
+}
+
+function swap(leftIdx, rightIdx, array) {
+    let temp = array[rightIdx];
+    array[rightIdx] = array[leftIdx];
+    array[leftIdx] = temp;
 }
