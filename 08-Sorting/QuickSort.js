3c0883cf5278388100cf1681a13246b6627e11ce
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-24 23:52:16 -0500

    quick sort

---
+++ 08-Sorting/QuickSort.js
@@ -0,0 +1,9 @@
+function quickSort(array) {
+    quiickSortHelper(array, 0, array.length - 1);
+    return array;
+}
+
+function quiickSortHelper(array, startIdx, endIdx) {
+    if (startIdx >= endIdx) return array;
+    
+}
